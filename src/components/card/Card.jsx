6ef810cdba08cb2ee7056c50b6aa245e2bc3480a
Manish Kumar Ray <manishray2233@gmail.com>
2023-09-10 19:49:04 +0530

    Done with recent post section

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,22 +6,22 @@ const Card = () => {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt='' fill />
+                <Image src="/p1.jpeg" alt='' fill  className={styles.image}/>
             </div>
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>11.02.2023</span>
+                    <span className={styles.date}>11.02.2023 - </span>
                     <span className={styles.category}>CULTURE</span>
                 </div>
-                <Link href="/">
+                <Link href="/"> 
                     <h1>What is Lorem Ipsum?</h1>
                 </Link>
-                <p>
+                <p className={styles.desc}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                     Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
                     when an unknown printer took a galley of type and scrambled it to make a type specimen book.....
                 </p>
-                <Link href="/">Read More</Link>
+                <Link href="/" className={styles.link}>Read More</Link>
             </div>
         </div>
     )
