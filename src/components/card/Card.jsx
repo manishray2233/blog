79cda5b25fd72088239cfcd5bb1de88118328fcc
Manish Kumar Ray <manishray2233@gmail.com>
2023-10-18 18:43:27 +0530

    Done with Pagination

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,19 +2,19 @@ import Image from "next/image"
 import styles from "./card.module.css"
 import Link from "next/link"
 
-const Card = () => {
+const Card = ({ key, item }) => {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} key={key}>
             <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt='' fill  className={styles.image}/>
+                <Image src="/p1.jpeg" alt='' fill className={styles.image} />
             </div>
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
                     <span className={styles.date}>11.02.2023 - </span>
                     <span className={styles.category}>CULTURE</span>
                 </div>
-                <Link href="/"> 
-                    <h1>What is Lorem Ipsum?</h1>
+                <Link href="/">
+                    <h1>{item.title}</h1>
                 </Link>
                 <p className={styles.desc}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
