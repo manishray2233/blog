935e894963fc9e5bc3affb95a5f91897bc52fe4b
Manish Kumar Ray <manishray2233@gmail.com>
2023-10-18 19:04:10 +0530

    Making the posts and blog page dynamic

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,30 +1,33 @@
-import Image from "next/image"
-import styles from "./card.module.css"
-import Link from "next/link"
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
 
 const Card = ({ key, item }) => {
     return (
         <div className={styles.container} key={key}>
-            <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt='' fill className={styles.image} />
-            </div>
+            {item.img && (
+                <div className={styles.imageContainer}>
+                    <Image src={item.img} alt="" fill className={styles.image} />
+                </div>
+            )}
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>11.02.2023 - </span>
-                    <span className={styles.category}>CULTURE</span>
+                    <span className={styles.date}>
+                        {item.createdAt.substring(0, 10)} -{" "}
+                    </span>
+                    <span className={styles.category}>{item.catSlug}</span>
                 </div>
-                <Link href="/">
+                <Link href={`/posts/${item.slug}`}>
                     <h1>{item.title}</h1>
                 </Link>
-                <p className={styles.desc}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.....
-                </p>
-                <Link href="/" className={styles.link}>Read More</Link>
+                {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
+                <div className={styles.desc} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }} />
+                <Link href={`/posts/${item.slug}`} className={styles.link}>
+                    Read More
+                </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -4,8 +4,8 @@ import Pagination from "@/components/pagination/Pagination"
 import Image from 'next/image';
 import Card from '../card/Card';
 
-const getData = async (page) => {
-    const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+const getData = async (page, cat) => {
+    const res = await fetch(`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`, {
         cache: "no-store"
     });
 
@@ -16,8 +16,8 @@ const getData = async (page) => {
     return res.json();
 }
 
-const CardList = async ({ page }) => {
-    const { posts, count } = await getData(page);
+const CardList = async ({ page, cat }) => {
+    const { posts, count } = await getData(page, cat);
 
     const POST_PER_PAGE = 2;
 
